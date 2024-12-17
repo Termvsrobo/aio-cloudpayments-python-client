73e7f8c7da2022d22554009a7fee24a8f27b87ed
Роман Соколов <145982699+ex3lite@users.noreply.github.com>
2024-12-17 10:23:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,28 +11,23 @@ long_description = open('README.rst', 'rt', encoding='utf8').read()
 long_description = re.sub(r'<(.*)#.*>`_', '<\g<1>>`_', long_description)
 
 setup(
-    name = 'cloudpayments',
-    packages = ['cloudpayments'],
+    name='aio-cloudpayments',  # Переименовано, чтобы указать асинхронную природу
+    packages=['cloudpayments'],
 
-    description = 'CloudPayments Python Client Library',
-    long_description = long_description,
+    description='CloudPayments Python Client Library (Asynchronous version)',
+    long_description=long_description,
 
-    version = VERSION,
+    version=VERSION,
 
-    author = 'Antida software',
-    author_email = 'info@antidasoftware.com',
-    license = 'MIT license',
+    author='Antida software',
+    author_email='info@antidasoftware.com',
+    license='MIT license',
 
-    url = 'https://github.com/antidasoftware/cloudpayments-python-client',
-    download_url = 'https://github.com/antidasoftware/cloudpayments-python-client/tarball/%s' % VERSION,
+    url='https://github.com/ex3lite/aio-cloudpayments-python-client',  # Ссылка на ваш форк
+    download_url=f'https://github.com/ex3lite/aio-cloudpayments-python-client/tarball/{VERSION}',
 
-    requires = [
-        'requests (>=2.9.1)',
-        'pytz (>=2015.7)'
-    ],
-
-    install_requires = [
-        'requests >=2.9.1',
+    install_requires=[
+        'aiohttp >=3.7.0',  # Асинхронная зависимость для HTTP-запросов
         'pytz >=2015.7'
     ],
 
@@ -41,15 +36,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',  # Обновлено
         'Topic :: Office/Business',
         'Topic :: Office/Business :: Financial',
         'Topic :: Software Development :: Libraries :: Python Modules'
